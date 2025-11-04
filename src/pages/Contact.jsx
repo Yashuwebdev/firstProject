f642e85be39c1e894e6e7f19bd121783726bf658
Yashuwebdev <yashiii1907@gmail.com>
2025-11-04 16:45:55 +0530

    change in contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -60,6 +60,12 @@ function Contact() {
                             </div>
                     {/* </form> */}
                         </div>
+
+
+
+
+
+                        
                 </div>
             </div>
         </>
